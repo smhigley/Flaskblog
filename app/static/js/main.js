4b7344b939b9d4c5bd7b875e03d3ee2930947108
smhigley <sarahmhigley@gmail.com>
2015-06-26 04:27:26 +0530

    map sidebar

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,27 @@
 (function() {
   'use strict';
 
+  FastClick.attach(document.body);
+
+  var map_el = document.getElementById('map'),
+      latitude = parseFloat(map_el.getAttribute('data-lat')),
+      longitude = parseFloat(map_el.getAttribute('data-long'));
+
+  var map = new ol.Map({
+    target: 'map',
+    controls: [],
+    interactions: [],
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.Stamen({
+          layer: 'watercolor'
+        })
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 5
+    })
+  });
+
 })();
