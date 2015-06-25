711e06110c64c3e3bf6fbf82cb895c696394f6a6
smhigley <sarahmhigley@gmail.com>
2015-06-26 04:26:08 +0530

    removed venv from git, added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+  name='TheLongTrip',
+  version='1.0',
+  description='A travel blog made with Flask and hope',
+  long_description=__doc__,
+  url='git@github.com:smhigley/Flaskblog.git',
+  author='Sarah Higley'
+  zip_safe=False,
+  install_requires=[
+    'Flask==0.10.1',
+    'Flask-Login==0.2.11',
+    'Flask-OAuth==0.12',
+    'Flask-Mail==0.9.1',
+    'Flask-SQLAlchemy==2.0',
+    'sqlalchemy-migrate==0.9.2',
+    'Flask-WTF==0.10.3',
+    'Flask-WhooshAlchemy==0.56',
+    'Flask-Babel==0.9',
+    'guess-language==0.2',
+    'flipflop==1.0',
+    'coverage==3.7.1'
+  ]
+)
